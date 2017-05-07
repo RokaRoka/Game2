47899ed8a064c099461acc35c1a6aab26b245e41
samscott <samuel.scott@mymail.champlain.edu>
2017-05-07 15:58:18 -0400

    Add player idle. Start work on house asset and shadow shader. Changes to tilemap_outside.

---
+++ shadowShader.lua
@@ -0,0 +1,21 @@
+shaderShadow =  love.graphics.newShader[[
+	extern vec2 light_pos;
+	extern vec2 object_pos;
+
+	vec4 effect( vec4 color, Image texture, vec2 texture_coords, vec2 screen_coords ){
+		vec4 pixel = Texel(texture, texture_coords);//This is the current pixel color
+		
+		//light radius
+		number radius = 128;
+		
+		
+		
+		if (length(screen_coords - light_pos) < radius) {
+			//brighten the ground smoothly
+			number t = (length(screen_coords - light_pos)/radius)* 0.9;
+			return mix(vec4(0.8, 0.8, 0.7, 1.0), pixel * color, t);
+		} else {
+			return pixel * color;
+		}
+	}
+]]
